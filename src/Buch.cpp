bfe02bc26ccb8dd283090cfb5764a84b9813bdf5
MAkinTurkes <pir2h@134.61.93.32>
2026-06-27 17:14:51 +0200

    Book class was added and all the necessary changes for that were made. Furthmore, the newly added functions for the Book class were documented.

---
+++ src/Buch.cpp
@@ -0,0 +1,21 @@
+/*
+ * Buch.cpp
+ *
+ *  Created on: Jun 27, 2026
+ *      Author: pir2h
+ */
+
+#include "Buch.h"
+#include "Medium.h"
+#include<string>
+using namespace std;
+
+Buch::Buch(string initTitel = "", string initAutor = "") : titel(initTitel), autor(initAutor)
+{
+}
+
+virtual void Buch::ausgabe() const
+{
+	Medium::ausgabe();
+	cout << "Autor: " << autor << endl;
+}
---
+++ src/Buch.h
@@ -0,0 +1,39 @@
+/*
+ * Buch.h
+ *
+ *  Created on: Jun 27, 2026
+ *      Author: pir2h
+ */
+
+#ifndef BUCH_H_
+#define BUCH_H_
+#include "Medium.h"
+#include<string>
+using namespace std;
+
+class Buch : public Medium
+{
+public:
+	/*
+	 * @brief Der Konstruktor fuer die Unterklasse Buch.
+	 *
+	 * @param initTitel Titel des Buchs
+	 * @param initAutor Autor des Buchs
+	 */
+	Buch(string initTitel, string initAutor);
+
+	/*
+	 * @brief Die Ausgabefunktion
+	 *
+	 * Diese Funktion gibt alle Informationen eines Buchss auf der Konsole aus
+	 */
+	virtual void ausgabe() const;
+
+private:
+	/*
+	 * @brief Autor des Buchs
+	 */
+	string autor;
+};
+
+#endif /* BUCH_H_ */
